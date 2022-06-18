b843a1a6e86c08f256f3bf78f2396b8e425896fa
John Obadare <98330860+johnalao@users.noreply.github.com>
2022-06-18 19:39:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("where do you want to go? Please type left or right.")
+direction_case = direction.lower()
+if direction_case == "left":
+	boat = input("You are in front of a river do you want to swim or wait for the boat?Please type swim or wait.")
+	boat_case = boat.lower()
+	if boat_case == "wait":
+		door = input("You are in front of two doors, red, blue, or yellow which are you taking? Please type Red or Blue or Yellow.")
+		door_case = door.lower()
+		if door_case == "yellow":
+			print("You have found the treasure")
+		else:
+			print("Sorry you have opened the door for a tiger. Game Over")
+	else:
+		print("Unfortunately, the tide of the water if too much. Game over")
+else:
+	print("You have opened the door to fire. Game over")
